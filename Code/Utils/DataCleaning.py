8c0522cbb71061738ced42d937ee411652d07d94
sushma <sushma23042001@gmail.com>
2024-06-26 00:36:58 -0400

    added custom functions for model training, model testing, pre-defined model training, finding new data point

--- Code/Utils/DataCleaning.py
+++ Code/Utils/DataCleaning.py
@@ -9,23 +9,28 @@ from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, recall
 import numpy as np
 import pickle as pkl
 import datetime as dt
+from Pickle import Pickle
+from Joblib import Joblib
 
 class DataCleaning:
     
     def __init__(self) -> None:
         pass
 
-    def pickleModel(model : any, project_name : str, model_name : str):
-        # store data in a pkl file
-        pkl.dump(model , open('PickledModels\\' + project_name + '_' + model_name + str(dt.datetime.now().strftime("%d%m%Y%H%M%S")) +'.pk1' , 'wb'))
-        print('Model Saved Successfully in PickledModels Folder')
+    # def pickleModel(model : any, project_name : str, model_name : str):
+    #     # store data in a pkl file
+    #     pkl.dump(model , open('PickledModels\\' + project_name + '_' + model_name + str(dt.datetime.now().strftime("%d%m%Y%H%M%S")) +'.pk1' , 'wb'))
+    #     print('Model Saved Successfully in PickledModels Folder')
 
-    def getPickledModle(file_name : str):
-        model = pkl.load(open(r'PickledModels/'+file_name+'.pk1' , 'rb'))
-        return model
+    # def getPickledModle(file_name : str):
+    #     model = pkl.load(open(r'PickledModels/'+file_name+'.pk1' , 'rb'))
+    #     return model
 
     project_name = 'FlightDelayPredecation'
     pd.set_option('display.max_columns', None)
+
+    pck = Pickle()
+    job = Joblib()
 
     df = pd.read_excel(r'C:\Users\kiran\Downloads\cleaned_data.xlsx')    
     print(df.shape)
@@ -101,85 +106,88 @@ class DataCleaning:
     print("Test RMSE:", test_rmse)
     print("Test R2:", test_r2)
 
-    pickleModel(model, project_name, model_name)
+    pck.save_model_to_pickle_folder(model, model_name)
+    job.save_model_to_joblib_folder(model, model_name)
+
+
 
     # Lasso regression (L1)
-    model_name = 'LassoRegression'
-    lasso = Lasso(alpha=0.1)
-    lasso.fit(x_train, y_train)
-    y_train_pred_lasso = lasso.predict(x_train)
-    y_test_pred_lasso = lasso.predict(x_test)
+    # model_name = 'LassoRegression'
+    # lasso = Lasso(alpha=0.1)
+    # lasso.fit(x_train, y_train)
+    # y_train_pred_lasso = lasso.predict(x_train)
+    # y_test_pred_lasso = lasso.predict(x_test)
 
-    r2_train_lasso = r2_score(y_train, y_train_pred_lasso)
-    r2_test_lasso = r2_score(y_test, y_test_pred_lasso)
+    # r2_train_lasso = r2_score(y_train, y_train_pred_lasso)
+    # r2_test_lasso = r2_score(y_test, y_test_pred_lasso)
 
-    rmse_train_lasso = np.sqrt(mean_squared_error(y_train, y_train_pred_lasso))
-    rmse_test_lasso = np.sqrt(mean_squared_error(y_test, y_test_pred_lasso))
+    # rmse_train_lasso = np.sqrt(mean_squared_error(y_train, y_train_pred_lasso))
+    # rmse_test_lasso = np.sqrt(mean_squared_error(y_test, y_test_pred_lasso))
 
-    print()
-    print("Lasso Regression:")
-    print(f"Train R2: {r2_train_lasso}")
-    print(f"Test R2: {r2_test_lasso}")
-    print(f"Train RMSE: {rmse_train_lasso}")
-    print(f"Test RMSE: {rmse_test_lasso}")
+    # print()
+    # print("Lasso Regression:")
+    # print(f"Train R2: {r2_train_lasso}")
+    # print(f"Test R2: {r2_test_lasso}")
+    # print(f"Train RMSE: {rmse_train_lasso}")
+    # print(f"Test RMSE: {rmse_test_lasso}")
 
-    pickleModel(lasso, project_name, model_name)
+    # pickleModel(lasso, project_name, model_name)
 
-    #Ridge regression (L2)
-    model_name = 'RidgeRegression'
-    ridge = Ridge(alpha=1.0)
-    ridge.fit(x_train, y_train)
-    y_train_pred_ridge = ridge.predict(x_train)
-    y_test_pred_ridge = ridge.predict(x_test)
+    # #Ridge regression (L2)
+    # model_name = 'RidgeRegression'
+    # ridge = Ridge(alpha=1.0)
+    # ridge.fit(x_train, y_train)
+    # y_train_pred_ridge = ridge.predict(x_train)
+    # y_test_pred_ridge = ridge.predict(x_test)
 
-    r2_train_ridge = r2_score(y_train, y_train_pred_ridge)
-    r2_test_ridge = r2_score(y_test, y_test_pred_ridge)
+    # r2_train_ridge = r2_score(y_train, y_train_pred_ridge)
+    # r2_test_ridge = r2_score(y_test, y_test_pred_ridge)
 
-    rmse_train_ridge = np.sqrt(mean_squared_error(y_train, y_train_pred_ridge))
-    rmse_test_ridge = np.sqrt(mean_squared_error(y_test, y_test_pred_ridge))
-    print()
-    print("Ridge Regression:")
-    print(f"Train R2: {r2_train_ridge}")
-    print(f"Test R2: {r2_test_ridge}")
-    print(f"Train RMSE: {rmse_train_ridge}")
-    print(f"Test RMSE: {rmse_test_ridge}")
+    # rmse_train_ridge = np.sqrt(mean_squared_error(y_train, y_train_pred_ridge))
+    # rmse_test_ridge = np.sqrt(mean_squared_error(y_test, y_test_pred_ridge))
+    # print()
+    # print("Ridge Regression:")
+    # print(f"Train R2: {r2_train_ridge}")
+    # print(f"Test R2: {r2_test_ridge}")
+    # print(f"Train RMSE: {rmse_train_ridge}")
+    # print(f"Test RMSE: {rmse_test_ridge}")
 
-    pickleModel(ridge, project_name, model_name)
+    # pickleModel(ridge, project_name, model_name)
 
-    #Decision Tree
-    model_name = 'DecisionTree'
-    # Step 5: Building the Decision Tree Model
-    dt_model = DecisionTreeClassifier()
-    # Step 6: Training the Model
-    dt_model.fit(x_train, y_train)
-    # Step 7: Evaluating the Model
-    y_pred = dt_model.predict(x_test)
-    print()
-    print("Decision Tree:")
-    print("Accuracy:", accuracy_score(y_test, y_pred))
+    # #Decision Tree
+    # model_name = 'DecisionTree'
+    # # Step 5: Building the Decision Tree Model
+    # dt_model = DecisionTreeClassifier()
+    # # Step 6: Training the Model
+    # dt_model.fit(x_train, y_train)
+    # # Step 7: Evaluating the Model
+    # y_pred = dt_model.predict(x_test)
+    # print()
+    # print("Decision Tree:")
+    # print("Accuracy:", accuracy_score(y_test, y_pred))
 
-    pickleModel(dt_model, project_name, model_name)
+    # pickleModel(dt_model, project_name, model_name)
 
-    #Random Forest
-    model_name = 'RandomForest'
-    rf = RandomForestClassifier(
-            n_estimators=100,    # More trees (can be tuned using cross-validation)
-            max_depth=6,        # Shallower trees
-            min_samples_split=4, # More samples required to split a node
-            min_samples_leaf=2,  # More samples required at a leaf node
-            bootstrap=True,      # Use bootstrapping
-            max_features='sqrt'  # Number of features for best split
-        )
+    # #Random Forest
+    # model_name = 'RandomForest'
+    # rf = RandomForestClassifier(
+    #         n_estimators=100,    # More trees (can be tuned using cross-validation)
+    #         max_depth=6,        # Shallower trees
+    #         min_samples_split=4, # More samples required to split a node
+    #         min_samples_leaf=2,  # More samples required at a leaf node
+    #         bootstrap=True,      # Use bootstrapping
+    #         max_features='sqrt'  # Number of features for best split
+    #     )
 
-    rf.fit(x_train, y_train)
+    # rf.fit(x_train, y_train)
 
-    rf_train_pred = rf.predict(x_train)
-    rf_y_pred = rf.predict(x_test)
-    train_acc = accuracy_score(y_train, rf_train_pred)
-    test_acc = accuracy_score(y_test, rf_y_pred)
-    print()
-    print("Random Forest:")
-    print("Train Accuracy: ", train_acc)
-    print("Test Accuracy: ", test_acc)
+    # rf_train_pred = rf.predict(x_train)
+    # rf_y_pred = rf.predict(x_test)
+    # train_acc = accuracy_score(y_train, rf_train_pred)
+    # test_acc = accuracy_score(y_test, rf_y_pred)
+    # print()
+    # print("Random Forest:")
+    # print("Train Accuracy: ", train_acc)
+    # print("Test Accuracy: ", test_acc)
 
-    pickleModel(rf, project_name, model_name)
+    # pickleModel(rf, project_name, model_name)
